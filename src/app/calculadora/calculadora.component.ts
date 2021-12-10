7c3f0011dd0a56f681ec3ed8069be71dbd70cc40
HenriqueCole <henrique_c_fernandes@estudante.sesisenai.org.br>
2021-12-10 20:58:24 -0300

    repairs

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -14,18 +14,51 @@ export class CalculadoraComponent implements OnInit {
   showNumbers = ""
 
   clickedOnDiv(evento){
-    this.showNumbers += evento
+    this.showNumbers = this.showNumbers.toString();
     console.log('Meu número é: ', evento)
     console.log("My screen: ", this.showNumbers)
+
+    if (this.showNumbers === "+"){
+      this.deleteNumbers();
+    }
+
+    if (this.showNumbers === "-"){
+      this.deleteNumbers();
+    }
+
+    if (this.showNumbers === "x"){
+      this.deleteNumbers();
+    }
+
+    if (this.showNumbers === "/"){
+      this.deleteNumbers();
+    }
+    
+    if (this.showNumbers.substring(this.showNumbers.length - 1) === "+" && evento === "+"){
+      
+    } else if (this.showNumbers.substring(this.showNumbers.length - 1) === "-" && evento === "-") {
+
+    } else if (this.showNumbers.substring(this.showNumbers.length - 1) === "x" && evento === "x") {
+
+    } else if (this.showNumbers.substring(this.showNumbers.length - 1) === "/" && evento === "/"){
+
+    } else {
+      this.showNumbers += evento
+    }
+    
+    
+    
+    
+    
+    
+    
   }
   
   showResult() {
-    this.showNumbers = eval(this.showNumbers)
+    this.showNumbers = eval(this.showNumbers);
   }
 
   deleteNumbers(){
     this.showNumbers = ""
   }
-
-
 }
--- src/app/calculadora/visor/visor.component.ts
+++ src/app/calculadora/visor/visor.component.ts
@@ -11,5 +11,4 @@ export class VisorComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
